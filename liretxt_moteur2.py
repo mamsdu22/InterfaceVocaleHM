566d10acfb703681646b8f04863adc50b4ed256e
mamsdu22 <mamadouseye3@gmail.com>
2016-02-01 12:39:01 +0100

    rajout path en argument

--- liretxt_moteur2.py
+++ liretxt_moteur2.py
@@ -4,7 +4,7 @@ import os
 import sys
 txtfilepath = sys.argv[1]
 path = sys.argv[2]
-wavfilepath = "file"+"tts1"+".wav"
+wavfilepath = "file"+"tts2"+".wav"
 print(wavfilepath)
 cmd="../script/spc/tts.pl --config ../../data/podalydes/corpus_config.json --add-filter IS_LAST_SYL_OF_SENTENCE --add-filter IS_LAST_SYL_OF_BG --add-filter IS_IN_WORD_END --add-filter IS_SYLLABLE_DESCENDING --add-filter IS_SYLLABLE_RISING --add-filter HAS_CODA --add-filter IS_IN_CODA --add-filter IS_SYLLABLE_END  --candidates-threshold=10 --fuzzy-sandwich --length 2"+ txtfilepath+ " "+ path+wavfilepath
 print(cmd)

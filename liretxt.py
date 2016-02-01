c3d5dc3e4826e91cb13805315b7c7487d5a6f18e
mamsdu22 <mamadouseye3@gmail.com>
2016-02-01 12:36:18 +0100

    rajout path en argument

--- liretxt.py
+++ liretxt.py
@@ -3,8 +3,9 @@
 import os
 import sys
 txtfilepath = sys.argv[1]
+path = sys.argv[2]
 wavfilepath = "file"+"tts1"+".wav"
 print(wavfilepath)
-cmd="../script/spc/tts.pl --config ../../data/podalydes/corpus_config.json --add-filter IS_LAST_SYL_OF_SENTENCE --add-filter IS_LAST_SYL_OF_BG --length 2 " + txtfilepath + " "+ wavfilepath
+cmd="../script/spc/tts.pl --config ../../data/podalydes/corpus_config.json --add-filter IS_LAST_SYL_OF_SENTENCE --add-filter IS_LAST_SYL_OF_BG --length 2 " + txtfilepath + " "+ path+wavfilepath
 print(cmd)
 os.system(cmd)

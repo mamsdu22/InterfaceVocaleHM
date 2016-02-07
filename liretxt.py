57b56a6fe3f4a3ec6f912ef7ef10e753560ffb1a
mamsdu22 <mamadouseye3@gmail.com>
2016-02-07 12:33:03 +0100

    blabla

--- liretxt.py
+++ liretxt.py
@@ -8,6 +8,6 @@ tab = txtfilepath.split("/")
 print(tab)
 wavfilepath = tab[len(tab)-1]+"tts1"+".wav"
 print(wavfilepath)
-cmd="../script/spc/tts.pl --config ../../data/podalydes/corpus_config.json --add-filter IS_LAST_SYL_OF_SENTENCE --add-filter IS_LAST_SYL_OF_BG --length 2 " + txtfilepath + " "+ path+wavfilepath
+cmd="/vrac/ivhm/speech/script/spc/tts.pl --config ../../data/podalydes/corpus_config.json --add-filter IS_LAST_SYL_OF_SENTENCE --add-filter IS_LAST_SYL_OF_BG --length 2 " + txtfilepath + " "+ path+wavfilepath
 print(cmd)
 os.system(cmd)

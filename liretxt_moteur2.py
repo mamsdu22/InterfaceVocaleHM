599050ff715e5d52926389a73d6adb64a5bd9961
mamsdu22 <mamadouseye3@gmail.com>
2016-02-07 16:52:49 +0100

    hope que ça marche

--- liretxt_moteur2.py
+++ liretxt_moteur2.py
@@ -7,6 +7,6 @@ path = sys.argv[2]
 tab = txtfilepath.split("/")
 wavfilepath = tab[len(tab)-1]+"tts2"+".wav"
 print(wavfilepath)
-cmd="/vrac/ivhm/speech/script/spc/tts.pl --config /vrac/ivhm/data/podalydes/corpus_config.json --add-filter IS_LAST_SYL_OF_SENTENCE --add-filter IS_LAST_SYL_OF_BG --add-filter IS_IN_WORD_END --add-filter IS_SYLLABLE_DESCENDING --add-filter IS_SYLLABLE_RISING --add-filter HAS_CODA --add-filter IS_IN_CODA --add-filter IS_SYLLABLE_END  --candidates-threshold=10 --fuzzy-sandwich --length 2"+ txtfilepath+ " "+ path+wavfilepath
+cmd="/vrac/ivhm/speech/script/spc/tts.pl --config /vrac/ivhm/data/podalydes/corpus_config.json --add-filter IS_LAST_SYL_OF_SENTENCE --add-filter IS_LAST_SYL_OF_BG --add-filter IS_IN_WORD_END --add-filter IS_SYLLABLE_DESCENDING --add-filter IS_SYLLABLE_RISING --add-filter HAS_CODA --add-filter IS_IN_CODA --add-filter IS_SYLLABLE_END  --candidates-threshold=10 --fuzzy-sandwich --length 2 " + txtfilepath+ " "+ path+wavfilepath
 print(cmd)
 os.system(cmd)
